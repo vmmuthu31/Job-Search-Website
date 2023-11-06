ad963a053a0d998cc73689ec41c9879c4f023a23
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-11-06 12:42:24 +0530

    updated the controllers,models, security file for the backend

--- Backend/src/main/java/com/example/jobsearch/repository/JobRepository.java
+++ Backend/src/main/java/com/example/jobsearch/repository/JobRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findByPostedBy(Long postedBy);
 }
+
